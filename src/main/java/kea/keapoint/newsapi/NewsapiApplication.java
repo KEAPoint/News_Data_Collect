c0bc6db8f5749710554da74a58b3bbb2dfcc2562
SeungHyeon <namsh1125@naver.com>
2023-10-19 00:31:18 +0900

    Eureka client 설정

--- src/main/java/kea/keapoint/newsapi/NewsapiApplication.java
+++ src/main/java/kea/keapoint/newsapi/NewsapiApplication.java
@@ -2,10 +2,12 @@ package kea.keapoint.newsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
+@SpringBootApplication
 public class NewsapiApplication {
 
     public static void main(String[] args) {

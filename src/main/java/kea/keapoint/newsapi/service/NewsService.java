0de1510623e84141b4d0a87745aee89682b59624
SeungHyeon <namsh1125@naver.com>
2023-10-23 23:43:16 +0900

    [Update] 워드 클라우드 정확도 향상 - 수집데이터 변경 (bitcoin -> cnn, bbc, forbes 3사) - 필터링 하는 단어 추가

--- src/main/java/kea/keapoint/newsapi/service/NewsService.java
+++ src/main/java/kea/keapoint/newsapi/service/NewsService.java
@@ -31,7 +31,7 @@ public class NewsService {
     private final NewsRepository newsRepository;
 
     private static final String API_KEY = "0aab704ca0444158bbf24e09a74b9918";
-    private static final String NEWS_API_URL = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=" + API_KEY;
+    private static final String NEWS_API_URL = "https://newsapi.org/v2/everything?domains=forbes.com,bbc.com,cnn.com&language=en&apiKey=" + API_KEY;
 
     @Transactional
     @Scheduled(fixedRate = 1500000)
@@ -86,11 +86,11 @@ public class NewsService {
         log.info(titleList.toString());
 
         // word press에 포함하지 않을 단어
-        Set<String> stopWords = new HashSet<>(Arrays.asList("the", "is", "at", "which"));
+        Set<String> stopWords = new HashSet<>(Arrays.asList(".", ",", "!", "their", "a", "other", "so", "that", "being", "between", "ours", "yourselves", "what", "if", "each", "haven", "you", "they", "not", "because", "against", "mustn't", "them", "over", "you're", "are", "won't", "its", "off", "am", "with", "there", "needn't", "hadn't", "about", "nor", "just", "by", "should", "needn", "shan", "until", "that'll", "more", "you'll", "you've", "will", "mightn", "you'd", "up", "yours", "through", "hasn't", "ma", "my", "such", "itself", "under", "too", "which", "during", "was", "than", "an", "been", "down", "these", "should've", "how", "herself", "the", "here", "to", "having", "don", "t", "theirs", "does", "on", "both", "wouldn't", "don't", "only", "who", "when", "were", "into", "any", "shouldn", "and", "why", "couldn't", "haven't", "or", "where", "she", "me", "him", "at", "below", "his", "her", "then", "did", "this", "he", "it's", "few", "most", "can", "mustn", "very", "s", "himself", "again", "ll", "yourself", "further", "doing", "some", "but", "i", "couldn", "aren't", "it", "now", "ourselves", "re", "have", "after", "our", "your", "out", "wasn't", "mightn't", "themselves", "whom", "once", "is", "hadn", "y", "doesn't", "weren", "we", "before", "as", "o", "own", "above", "wouldn", "of", "all", "shan't", "be", "weren't", "while", "d", "from", "aren", "isn", "won", "didn", "no", "hasn", "those", "didn't", "isn't", "she's", "ain", "same", "shouldn't", "do", "m", "for", "hers", "had", "doesn", "myself", "in", "wasn", "has", "ve"));
 
         // news title에서 단어 단위로 쪼개 어떤 단어가 몇번 사용되었는지 추출
         Map<String, Long> wordFrequency = titleList.stream()
-                .flatMap(title -> Arrays.stream(title.split(" ")))
+                .flatMap(title -> Arrays.stream(title.toLowerCase().split("\\W+")))
                 .filter(word -> !stopWords.contains(word))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         log.info(wordFrequency.toString());
